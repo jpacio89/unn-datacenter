fbbdb8d9130e07f2fe2c123fb9668605a4998a81
João Coelho <admin.henyah@gmail.com>
2020-07-10 00:12:20 +0200

    Adding spark server

--- src/main/java/com/unn/datacenter/Main.java
+++ src/main/java/com/unn/datacenter/Main.java
@@ -3,9 +3,13 @@ package com.unn.datacenter;
 import retrofit2.GsonConverterFactory;
 import retrofit2.Retrofit;
 
+import static spark.Spark.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.github.com/")
                 .addConverterFactory(GsonConverterFactory.create())
