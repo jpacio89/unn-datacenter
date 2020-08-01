ccfc498e631213aa58e78b2186220ab7f0be5a4d
Joao Coelho <joao.coelho@bontouch.com>
2020-08-01 16:34:12 +0100

    no message

--- src/main/java/com/unn/datacenter/models/Body.java
+++ src/main/java/com/unn/datacenter/models/Body.java
@@ -1,4 +1,16 @@
 package com.unn.datacenter.models;
 
 public class Body {
+    Row[] rows;
+
+    public Body() {
+
+    }
+
+    Body withRows(Row[] rows) {
+        this.rows = rows;
+        return this;
+    }
+
+
 }
